ce49b9a727bfe8f8b1f21d787f3a4ca1a82380a7
wanderer911 <dp160291sas@gmail.com>
2019-06-14 15:25:23 +0300

    variable name changes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function wrapper() {
 	const elements = {};
 	const f = (id) => {
-		const obj = $w(id);
-		if (Array.isArray(obj)) { //multiple select
-			const methods = Object.getOwnPropertyNames(obj).filter(prop => typeof obj[prop] === 'function' && prop.slice(0, 2) === 'on');
+		const result = $w(id);
+		if (Array.isArray(result)) { //multiple select
+			const methods = Object.getOwnPropertyNames(result).filter(prop => typeof result[prop] === 'function' && prop.slice(0, 2) === 'on');
 			return methods.reduce((acc, cur) => {
 				acc[cur] = (func) => {
-					obj.forEach((element => {
+					result.forEach((element => {
 						if (!elements[`${element.uniqueId}.${cur}`]) {
 							elements[`${element.uniqueId}.${cur}`] = func;
 							$w(`#${element.id}`)[cur]((...props) => elements[`${element.uniqueId}.${cur}`](...props));
@@ -16,26 +16,26 @@ function wrapper() {
 					}));
 				}
 				return acc;
-			}, obj);
+			}, result);
 		}
-		const elementProto = Object.getPrototypeOf(obj);
+		const elementProto = Object.getPrototypeOf(result);
 		const nodeProto = Object.getPrototypeOf(elementProto);
 		const getMethods = (original, parent) => (
 			Object
 			.getOwnPropertyNames(parent)
 			.filter(prop => typeof original[prop] === 'function' && prop.slice(0, 2) === 'on'));
-		const methodsArray = [...getMethods(obj, elementProto), ...getMethods(obj, nodeProto)];
+		const methodsArray = [...getMethods(result, elementProto), ...getMethods(result, nodeProto)];
 		return methodsArray.reduce((acc, cur) => {
 			acc[cur] = (func) => {
-				if (!elements[`${obj.uniqueId}.${cur}`]) {
-					elements[`${obj.uniqueId}.${cur}`] = func;
-					$w(id)[cur]((...props) => elements[`${obj.uniqueId}.${cur}`](...props));
+				if (!elements[`${result.uniqueId}.${cur}`]) {
+					elements[`${result.uniqueId}.${cur}`] = func;
+					$w(id)[cur]((...props) => elements[`${result.uniqueId}.${cur}`](...props));
 				} else {
-					elements[`${obj.uniqueId}.${cur}`] = func;
+					elements[`${result.uniqueId}.${cur}`] = func;
 				}
 			}
 			return acc;
-		}, obj);
+		}, result);
 	}
 	f.at = $w.at;
 	f.onReady = $w.onReady;
