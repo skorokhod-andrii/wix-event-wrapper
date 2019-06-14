ca1f86e266ba29580ef70c5e461126cbb940d368
wanderer911 <dp160291sas@gmail.com>
2019-06-14 15:17:02 +0300

    added support for multiple assignment

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ function wrapper() {
 	const elements = {};
 	const f = (id) => {
 		const obj = $w(id);
+		if (Array.isArray(obj)) { //multiple select
+			const methods = Object.getOwnPropertyNames(obj).filter(prop => typeof obj[prop] === 'function' && prop.slice(0, 2) === 'on');
+			return methods.reduce((acc, cur) => {
+				acc[cur] = (func) => {
+					obj.forEach((element => {
+						if (!elements[`${element.uniqueId}.${cur}`]) {
+							elements[`${element.uniqueId}.${cur}`] = func;
+							$w(`#${element.id}`)[cur]((...props) => elements[`${element.uniqueId}.${cur}`](...props));
+						} else {
+							elements[`${element.uniqueId}.${cur}`] = func;
+						}
+					}));
+				}
+				return acc;
+			}, obj);
+		}
 		const elementProto = Object.getPrototypeOf(obj);
 		const nodeProto = Object.getPrototypeOf(elementProto);
 		const getMethods = (original, parent) => (
