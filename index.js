c582535dce247c34ce775b9239a92d149453f30f
wanderer911 <dp160291sas@gmail.com>
2019-06-18 13:37:30 +0300

    variable name changes. Now element.id is also used in name of attribute

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function wrapper() {
 	const elements = {};
-	const f = (id) => {
-		const result = $w(id);
+	const f = (selector) => {
+		const result = $w(selector);
 		if (Array.isArray(result)) { //multiple select
 			const methods = Object.getOwnPropertyNames(result).filter(prop => typeof result[prop] === 'function' && prop.slice(0, 2) === 'on');
 			return methods.reduce((acc, cur) => {
 				acc[cur] = (func) => {
 					result.forEach((element => {
-						if (!elements[`${element.uniqueId}.${cur}`]) {
-							elements[`${element.uniqueId}.${cur}`] = func;
-							$w(`#${element.id}`)[cur]((...props) => elements[`${element.uniqueId}.${cur}`](...props));
+						if (!elements[`${element.id}.${cur}.${element.uniqueId}`]) {
+							elements[`${element.id}.${cur}.${element.uniqueId}`] = func;
+							$w(`#${element.id}`)[cur]((...props) => elements[`${element.id}.${cur}.${element.uniqueId}`](...props));
 						} else {
-							elements[`${element.uniqueId}.${cur}`] = func;
+							elements[`${element.id}.${cur}.${element.uniqueId}`] = func;
 						}
 					}));
 				}
@@ -20,25 +20,26 @@ function wrapper() {
 		}
 		const elementProto = Object.getPrototypeOf(result);
 		const nodeProto = Object.getPrototypeOf(elementProto);
-		const getMethods = (original, parent) => (
+		const getMethods = (child, parent) => (
 			Object
 			.getOwnPropertyNames(parent)
-			.filter(prop => typeof original[prop] === 'function' && prop.slice(0, 2) === 'on'));
+			.filter(prop => typeof child[prop] === 'function' && prop.slice(0, 2) === 'on'));
 		const methodsArray = [...getMethods(result, elementProto), ...getMethods(result, nodeProto)];
 		return methodsArray.reduce((acc, cur) => {
 			acc[cur] = (func) => {
-				if (!elements[`${result.uniqueId}.${cur}`]) {
-					elements[`${result.uniqueId}.${cur}`] = func;
-					$w(id)[cur]((...props) => elements[`${result.uniqueId}.${cur}`](...props));
+				if (!elements[`${result.id}.${cur}.${result.uniqueId}`]) {
+					elements[`${result.id}.${cur}.${result.uniqueId}`] = func;
+					$w(selector)[cur]((...props) => elements[`${result.id}.${cur}.${result.uniqueId}`](...props));
 				} else {
-					elements[`${result.uniqueId}.${cur}`] = func;
+					elements[`${result.id}.${cur}.${result.uniqueId}`] = func;
 				}
 			}
 			return acc;
 		}, result);
-	}
+	};
 	Object.getOwnPropertyNames($w).filter(prop => typeof $w[prop] === 'function').reduce((acc, cur) => { acc[cur] = $w[cur]; return acc }, f);
 	return f;
 }
 
-export default wrapper();
+const $ = wrapper();
+export default $;
